bcbc534905e38bd35d94cb676a04f5121011199c
José Gomes de Menezes <josegomesd6@gmail.com>
2025-02-06 07:23:25 -0300

    feat: add primevue and screen loading

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
-<script setup lang="ts">
+<script setup>
 import Header from "./components/Header.vue";
 import CardMain from "./components/CardMain.vue";
 import SocialIcons from "./components/SocialIcons.vue";
 import Experience from "./components/Experience.vue";
 import Work from "./components/Work.vue";
+import { ref, onMounted } from 'vue';
+import ProgressSpinner from "primevue/progressspinner";
+
+const loading = ref(true);
+
+onMounted(async () => {
+  setTimeout(() => {
+    loading.value = false;
+  }, 1500);
+});
 </script>
 
 <template>
-  <Header />
-  <CardMain />
-  <SocialIcons />
-  <Experience />
-  <Work />
+  <div v-if="loading" class="flex items-center justify-center h-screen">
+    <ProgressSpinner style="width: 50px; height: 50px" strokeWidth="8" fill="transparent" animationDuration=".5s"
+      aria-label="Loading" />
+  </div>
+  <div v-else>
+    <Header />
+    <CardMain />
+    <SocialIcons />
+    <Experience />
+    <Work />
+  </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import "./assets/main.css";
 import "normalize.css";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 import { createApp } from "vue";
 import App from "./App.vue";
 
 const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
 
 app.mount("#app");
